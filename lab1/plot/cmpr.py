3e8ff5925da7a064170eb3e236d4069fd4af055e
Janusz Witkowski <46355383+JanuszWitkowski@users.noreply.github.com>
2023-03-07 13:05:25 +0100

    Plot experiments for lambda

--- lab1/plot/cmpr.py
+++ lab1/plot/cmpr.py
@@ -13,18 +13,57 @@ def simple_compare (filename, name):
     # ns.insert(0, 0)
     values = [arr[1] for arr in data]
     # values.insert(0, 0.0)
-    # efunc = [math.e for _ in ns]
-    # zerofunc = [0.0 for _ in ns]
+    efunc = [math.e for _ in ns]
+    zerofunc = [0.0 for _ in ns]
     plt.figure().set_figwidth(18)
-    # plt.plot(ns, zerofunc, 'b')
-    # plt.plot(ns, efunc, 'r')
-    plt.plot(ns, values, 'g')
-    # plt.ylim(bottom=0)
+    # plt.plot(ns, zerofunc)
+    plt.plot(ns, efunc)
+    plt.plot(ns, values)
+    plt.ylim(bottom=0.0)
     plt.title(name)
     # plt.xlabel("Rozkład zmiennej losowej liczby slotów do wyboru lidera")
     # plt.ylabel("Częstotliwość występowania")
     # plt.savefig(name + '.png')
     plt.show()
+
+def lambda_compare (filename, name):
+    with open(filename) as file:
+        data = [line.rstrip().split() for line in file][1:]
+    # print(data)
+    us = [int(arr[0][:-1]) for arr in data]
+    ls = [int(arr[1][:-1]) for arr in data]
+    lambda_value = [0.579 for _ in us]
+    ntwo = [float(arr[2]) for arr in data]
+    n10 = [float(arr[3]) for arr in data]
+    n25 = [float(arr[4]) for arr in data]
+    n50 = [float(arr[5]) for arr in data]
+    n75 = [float(arr[6]) for arr in data]
+    n90 = [float(arr[7]) for arr in data]
+    nu = [float(arr[8]) for arr in data]
+    results = [arr[2:] for arr in data]
+
+    plt.figure(figsize=(16, 8))
+    plt.plot(us, lambda_value, label='lambda')
+    plt.plot(us, ntwo, label='n = 2')
+    plt.plot(us, n10, label='n = 0.1u')
+    plt.plot(us, n25, label='n = .25u')
+    plt.plot(us, n50, label='n = 0.5u')
+    plt.plot(us, n75, label='n = 0.75u')
+    plt.plot(us, n90, label='n = 0.9u')
+    plt.plot(us, nu, label='n = u')
+    plt.ylim(bottom=0.45)
+    plt.ylim(top=0.9)
+    plt.title(name)
+    # plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05), ncol=3)
+    plt.legend(loc='lower center', ncol=3)
+    plt.savefig(name + '.png')
+    plt.show()
+
+    # plt.clf()
+    # for arr in results:
+    #     plt.plot(arr)
+    # plt.plot(lambda_value[:6])
+    # plt.show()
 
 if __name__ == "__main__":
     args = sys.argv
@@ -39,3 +78,4 @@ if __name__ == "__main__":
             simple_compare(filename, name)
         elif name[0] == 'l':
             print("Lambda")
+            lambda_compare(filename, name)
