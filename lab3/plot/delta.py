8c4efaedf5f89a226730f981777f1093059386e2
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-04-18 11:53:01 +0200

    Plot HyperLogLog

--- lab3/plot/delta.py
+++ lab3/plot/delta.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 from sys import argv
 from typing import List, Tuple
-from math import sqrt, exp, nextafter, inf
+# import numpy.nextafter
+from math import sqrt, exp, inf
 
-def open_csv_for_numbers(filename: str) -> Tuple[List[int], List[float]]:
+def open_csv_for_numbers(filename: str) -> Tuple[List[int], List[float], List[float]]:
     with open(filename, 'r') as file:
         lines = [line.split(';') for line in file]
     ns = [int(line[0]) for line in lines]
-    ratios = [float(line[1]) for line in lines]
-    return ns, ratios
+    ests = [float(line[1]) for line in lines]
+    ratios = [float(line[2]) for line in lines]
+    return ns, ests, ratios
 
 def sort_ratios (ratios: List[float]) -> List[float]:
     return sorted(ratios, key=lambda x:x)
+
+def calculate_expected_value(ratios: List[float]) -> float:
+    n = len(ratios)
+    sum = 0
+    for r in ratios:
+        sum += r
+    return 1.0 * sum / n
 
 def calculate_variance (ratios: List[float], ev: float) -> float:
     sum = 0.0
@@ -28,17 +37,17 @@ def chernoff_help (delta: float, k: int):
     eps2 = delta / (1.0 + delta)
     return fk(eps2) + fk(-eps1)
 
-def calculate_delta_chernoff (k: int, alpha: float) -> float:
-    left = 0.0
-    right = 1.0
-    while left <= right:
-        mid = (left + right) / 2.0
-        res = chernoff_help(mid, k)
-        if res <= alpha:
-            right = nextafter(mid, 0.0)
-        else:
-            left = nextafter(mid, inf)
-    return left
+# def calculate_delta_chernoff (k: int, alpha: float) -> float:
+#     left = 0.0
+#     right = 1.0
+#     while left <= right:
+#         mid = (left + right) / 2.0
+#         res = chernoff_help(mid, k)
+#         if res <= alpha:
+#             right = nextafter(mid, 0.0)
+#         else:
+#             left = nextafter(mid, inf)
+#     return left
 
 def calculate_delta_in_estimation (sorted_ratios: List[float], alpha: float) -> float:
     ln = len(sorted_ratios)
@@ -64,7 +73,11 @@ if __name__ == "__main__":
     if len(argv) > 1:
         filename = argv[1]
         print(f"Given file: {filename}")
-        ns, ratios = open_csv_for_numbers(filename)
+        ns, ests, ratios = open_csv_for_numbers(filename)
+
+        ev = calculate_expected_value(ratios)
+        var = calculate_variance(ratios, ev)
+
         sorted_ratios = sort_ratios(ratios)
 
         delta1 = calculate_delta_in_estimation(sorted_ratios, alpha1)
@@ -75,16 +88,18 @@ if __name__ == "__main__":
         delta_czybyszew_2 = calculate_delta_czybyszew(k, alpha2)
         delta_czybyszew_3 = calculate_delta_czybyszew(k, alpha3)
 
-        delta_chernoff1 = calculate_delta_chernoff(k, alpha1)
-        delta_chernoff2 = calculate_delta_chernoff(k, alpha2)
-        delta_chernoff3 = calculate_delta_chernoff(k, alpha3)
+        # delta_chernoff1 = calculate_delta_chernoff(k, alpha1)
+        # delta_chernoff2 = calculate_delta_chernoff(k, alpha2)
+        # delta_chernoff3 = calculate_delta_chernoff(k, alpha3)
 
+        print(f"EV = {ev}")
+        print(f"Var = {var}")
         print(f"alpha = {alpha1} | delta = {delta1}")
         print(f"alpha = {alpha1} | czybyszew = {delta_czybyszew_1}")
-        print(f"alpha = {alpha1} | czernoff = {delta_chernoff1}")
+        # print(f"alpha = {alpha1} | czernoff = {delta_chernoff1}")
         print(f"alpha = {alpha2} | delta = {delta2}")
         print(f"alpha = {alpha2} | czybyszew = {delta_czybyszew_2}")
-        print(f"alpha = {alpha2} | czernoff = {delta_chernoff2}")
+        # print(f"alpha = {alpha2} | czernoff = {delta_chernoff2}")
         print(f"alpha = {alpha3} | delta = {delta3}")
         print(f"alpha = {alpha3} | czybyszew = {delta_czybyszew_3}")
-        print(f"alpha = {alpha3} | czernoff = {delta_chernoff3}")
+        # print(f"alpha = {alpha3} | czernoff = {delta_chernoff3}")
---
+++ lab3/plot/plot.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+from sys import argv
+from typing import List, Tuple
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from delta import sort_ratios, calculate_delta_in_estimation
+
+def proper_name (filename: str) -> str:
+    name = filename[:-4]
+    l = len(name)
+    idx = 0
+    while idx < l and name[-idx] != '/':
+        idx += 1
+    if idx == l:
+        return name
+    return name[-idx+1:]
+
+def open_csv_for_numbers(filename: str) -> Tuple[List[int], List[float], List[float]]:
+    with open(filename, 'r') as file:
+        lines = [line.split(';') for line in file]
+    ns = [int(line[0]) for line in lines]
+    ests = [float(line[1]) for line in lines]
+    ratios = [float(line[2]) for line in lines]
+    return ns, ests, ratios
+
+def plot_simple (title: str, ns: List[int], ratios: List[float]):
+    if len(ns) == len(ratios):
+        plt.figure(figsize=(16, 8))
+        plt.title(title)
+        plt.scatter(ns, ratios)
+        plt.xlabel("Wielkość multizbioru [n]")
+        plt.ylabel("estymata/n")
+        plt.savefig(title + ".png")
+        # plt.show()
+        print(f"Done for {title}!")
+    else:
+        print("ERROR: Different sizes of ns and ratios.")
+
+def plot_simple_multiple(title: str, filenames: List[str]):
+    plt.figure(figsize=(16, 8))
+    plt.title(title)
+
+    for filename in filenames:
+        ns, ests, ratios = open_csv_for_numbers(filename)
+        plt.scatter(ns, ratios, label=proper_name(filename), marker=".")
+
+    plt.xlabel("Wielkość multizbioru [n]")
+    plt.ylabel("estymata/n")
+    plt.legend()
+    plt.savefig(title + ".png")
+    print(f"Done for {title}!")
+
+def plot_hashes (title: str, bs: List[int], hashes: List[Tuple[List[float], str]]):
+    plt.figure(figsize=(16, 8))
+    plt.title(title)
+    for hash in hashes:
+        plt.scatter(bs, hash[0], label=hash[1], marker=".")
+    plt.legend()
+    plt.xlabel("Liczba bajtów")
+    plt.ylabel("Średnia różnica stosunku estymaty do rzeczywistej wartości od wartości oczekiwanej")
+    plt.savefig(title + ".png")
+    # plt.show()
+    print(f"Done for {title}!")
+
+def plot_deltas (title: str, ns: List[int], ratios: List[float], deltas: List[Tuple[float, str, str]]):
+    ln = len(ns)
+    if len(ns) == len(ratios):
+        filename = title
+        plt.figure(figsize=(16, 8))
+        plt.title(title)
+        plt.scatter(ns, ratios)
+        for delta in deltas:
+            plt.plot(ns, [(1.0 - delta[0]) for _ in range(ln)], label=delta[1], color=delta[2])
+            plt.plot(ns, [(1.0 + delta[0]) for _ in range(ln)], color=delta[2])
+            filename = filename + "_" + delta[1]
+        plt.xlabel("Wielkość multizbioru [n]")
+        plt.ylabel("estymata/n")
+        plt.legend()
+        plt.savefig(filename + ".png")
+        # plt.show()
+        print(f"Done for {filename}!")
+    else:
+        print("ERROR: Different sizes of ns and ratios.")
+
+
+if __name__ == "__main__":
+    if len(argv) > 2:
+        filenames = argv[1:]
+        print(filenames)
+        # for filename in filenames:
+        #     title = proper_name(filename)
+        #     ns, ests, ratios = open_csv_for_numbers(filename)
+        #     plot_simple(title, ns, ratios)
+        plot_simple_multiple("HyperLogLog_vs_MinCount", filenames)
+    elif len(argv) > 1:
+        filename = argv[1]
+        if "6.csv" in filename:
+            with open(filename, 'r') as file:
+                lines = [line.split(';') for line in file]
+            bs = [int(line[0]) for line in lines[1:]]
+            # print(bs)
+            names = [name for name in lines[0][1:]]
+            # print(names)
+            # print(len(names))
+            # print(len(lines[1:]))
+            hashes = [([float(lines[j+1][i+1]) for j in range(len(lines[1:]))], names[i]) for i in range(len(names))]
+            # print(hashes)
+            plot_hashes("hashes", bs, hashes)
+        else:
+            title = proper_name(filename)
+            ns, ests, ratios = open_csv_for_numbers(filename)
+            plot_simple(title, ns, ratios)
+    else:
+        filename = "../results/5b_k400.csv"
+        alpha = 0.005
+        k = 400
+        # title = proper_name(filename)
+        title = proper_name(filename) + "_alpha" + str(alpha)
+        ns, ratios = open_csv_for_numbers(filename)
+        sorted_ratios = sort_ratios([r for r in ratios])
+        # delta1 = calculate_delta_in_estimation(sorted_ratios, alpha1)
+        # delta2 = calculate_delta_in_estimation(sorted_ratios, alpha2)
+        # delta3 = calculate_delta_in_estimation(sorted_ratios, alpha3)
+        # deltas = [(delta1, "alpha-05", "red"),
+        #             (delta2, "alpha-01", "green"),
+        #             (delta3, "alpha-005", "black")]
+        deltas = [(calculate_delta_in_estimation(sorted_ratios, alpha), "empirum", "red"),
+                    (calculate_delta_czybyszew(k, alpha), "chebyschev", "green"),
+                    (calculate_delta_chernoff(k, alpha), "chernoff", "blue")]
+        plot_deltas(title, ns, ratios, deltas)
+
+
+
+# import pandas as pd
+# from matplotlib import pyplot as plt
+# plt.rcParams["figure.figsize"] = [7.00, 3.50]
+# plt.rcParams["figure.autolayout"] = True
+# columns = ["Name", "Marks"]
+# df = pd.read_csv("input.csv", usecols=columns)
+# print("Contents in csv file:
+# ", df)
+# plt.plot(df.Name, df.Marks)
+# plt.show()
