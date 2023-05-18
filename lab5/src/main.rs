1f80121b4bdbc3f13c3a37af4cb18de7fba49905
Janusz Witkowski <janusz.witkowski@vewd.com>
2023-05-18 16:27:18 +0200

    Implement ex10

--- lab5/src/main.rs
+++ lab5/src/main.rs
@@ -1,5 +1,117 @@
-mod processor;
+// mod processor;
+
+use std::env;
+use std::collections::HashSet;
+
+const DEFAULT_RING_SIZE:    usize = 5;
+
+fn is_illegal(config: &Vec<u8>) -> bool {
+    let ring_size = config.len();
+    let mut count = 0;
+    // First processor
+    if config[0] == config[ring_size - 1] {
+        count += 1;
+    }
+    // The rest of the ring
+    for i in 1..ring_size {
+        if config[i] != config[i - 1] {
+            count += 1;
+        }
+        if count == 2 {
+            return true;
+        }
+    }
+    false
+}
+
+fn get_configs(set: HashSet<Vec<u8>>, i: usize, ring_size: usize) -> HashSet<Vec<u8>> {
+    // println!("i={}", i);
+    if i == ring_size {
+        // println!("get_configs end!");
+        return set;
+    }
+    let mut new_set = HashSet::new();
+    for j in 0..=ring_size {
+        for config in &set {
+            let mut new_config = config.clone();
+            new_config[i] = j as u8;
+            new_set.insert(new_config);
+        }
+    }
+    // for config in &new_set {
+    //     println!("{:?}", config);
+    // }
+    // println!("---");
+    get_configs(new_set, i + 1, ring_size)
+}
+
+fn get_all_configs(ring_size: usize) -> HashSet<Vec<u8>> {
+    let mut set = HashSet::new();
+    set.insert(vec![0; ring_size]);
+    get_configs(set, 0, ring_size)
+}
+
+fn filter_configs(configs: HashSet<Vec<u8>>) -> HashSet<Vec<u8>> {
+    let mut new_set = HashSet::new();
+    for config in configs {
+        if is_illegal(&config) {
+            new_set.insert(config);
+        }
+    }
+    new_set
+}
+
+fn max_steps(ring_size: usize) -> usize {
+    let mut configs = filter_configs(get_all_configs(ring_size));
+    println!("Number of illegal configs: {}", configs.len());
+    let mut steps = 0;
+    println!("Starting...");
+    while !configs.is_empty() {
+        let mut new_configs = HashSet::new();
+        for config in &configs {
+            if config[0] == config[ring_size - 1] {
+                let mut new_one = config.clone();
+                new_one[0] = (new_one[0] + 1) % (ring_size as u8 + 1);
+                new_configs.insert(new_one);
+            }
+            for i in 1..ring_size {
+                if config[i] != config[i-1] {
+                    let mut new_one = config.clone();
+                    new_one[i] = new_one[i-1];
+                    new_configs.insert(new_one);
+                }
+            }
+        }
+        configs = HashSet::new();
+        for config in new_configs {
+            if is_illegal(&config) {
+                configs.insert(config);
+            }
+        }
+        steps += 1;
+        println!("Finished step {}", steps);
+    }
+    steps
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let ring_size: usize = match args.len() {
+        2 => {
+            match args[1].parse() {
+                Ok(n) => n,
+                _ => {
+                    println!("Argument doesn't have a correct type. Defaulting to {}.", DEFAULT_RING_SIZE);
+                    DEFAULT_RING_SIZE
+                }
+            }
+        },
+        _ => {
+            println!("Supply with ONE argument of type unsigned integer. Defaulting to {}", DEFAULT_RING_SIZE);
+            DEFAULT_RING_SIZE
+        }
+    };
+    println!("Ring size: {}", ring_size);
+    let max_steps = max_steps(ring_size);
+    println!("Max steps: {}", max_steps);
 }
---
+++ lab5/z10.jl
@@ -0,0 +1,95 @@
+using Dates
+
+global const RING_SIZE = 5
+
+function isIllegal(config)
+    count = 0
+
+    if config[1] == config[RING_SIZE]
+        count += 1
+    end
+
+    for i in 2:RING_SIZE
+        if config[i - 1] != config[i]
+            count += 1
+        end
+        if count == 2
+            return true
+        end
+    end
+
+    return false
+end
+
+function getConfigs(i, set)
+    if i == RING_SIZE + 1
+        return set
+    else
+        new_set = Set{Array{UInt8,1}}()
+
+        for j in 0:RING_SIZE
+            for config in set
+                new_config = copy(config)
+                new_config[i] = j
+                push!(new_set, new_config)
+            end
+        end
+
+        return getConfigs(i + 1, new_set)
+    end
+end
+
+function getAllConfigs()
+    timer = now()
+    set = Set{Array{UInt8,1}}()
+    push!(set, zeros(RING_SIZE))
+    configs = getConfigs(1, set)
+    println("Time of configs generation: ", canonicalize(Dates.CompoundPeriod(now() - timer)))
+    return configs
+end
+
+function maxSteps()
+    all_configs = getAllConfigs()
+    println("Number of all configs: ", length(all_configs))
+
+    timer = now()
+    configs = Set{Array{UInt8,1}}(
+        filter(config -> isIllegal(config), all_configs)
+    )
+    println("Number of illegal configs: ", length(all_configs))
+
+    steps = 0
+    println("Starting...")
+
+    while !isempty(configs)
+        new_configs = Set{Array{UInt8,1}}()
+
+        for config in configs
+            if config[1] == config[RING_SIZE]
+                new_one = copy(config)
+                new_one[1] = (new_one[1] + 1) % (RING_SIZE + 1)
+                push!(new_configs, new_one)
+            end
+
+            for i in 2:RING_SIZE
+                if config[i - 1] != config[i]
+                    new_one = copy(config)
+                    new_one[i] = new_one[i - 1]
+                    push!(new_configs, new_one)
+                end
+            end
+        end
+
+        configs = Set{Array{UInt8,1}}(
+            filter(config -> isIllegal(config), new_configs)
+        )
+
+        steps += 1
+        println("Finished step ", steps)
+    end
+
+    println("Time of simulation: ", canonicalize(Dates.CompoundPeriod(now() - timer)))
+    return steps
+end
+
+println("max_steps = ", maxSteps())
